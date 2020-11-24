18a1b4109a0901ab12abf9c49e11ce05cd467835
Panchis7u7 <smadrigal.rod@gmail.com>
2020-11-23 21:01:16 -0600

    Reto 1 completado.

--- Sesion7/Reto1.sql
+++ Sesion7/Reto1.sql
@@ -3,6 +3,14 @@
 CREATE DATABASE IF NOT EXISTS theater;
 USE theater;
 
+CREATE TABLE IF NOT EXISTS users (
+   id_usuario INT PRIMARY KEY, 
+   genero VARCHAR(1), 
+   edad INT,
+   ocup INT,
+   cp VARCHAR(20)
+);
+
 CREATE TABLE IF NOT EXISTS movies (
    id_pelicula INT PRIMARY KEY, 
    titulo VARCHAR(60), 
@@ -10,13 +18,16 @@ CREATE TABLE IF NOT EXISTS movies (
 ); 
 
 CREATE TABLE IF NOT EXISTS ratings (
+   id_rating INT PRIMARY KEY,
    id_usuario INT, 
    id_pelicula INT, 
    rating INT, 
    time_stamp BIGINT,
+   FOREIGN KEY (id_usuario) REFERENCES users (id_usuario) ON UPDATE CASCADE ON DELETE CASCADE,
    FOREIGN KEY (id_pelicula) REFERENCES movies (id_pelicula) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS ratings;
 DROP DATABASE IF EXISTS theater;
